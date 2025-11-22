9fd181dc89faf1c281e5c18561d0387775e99c5d
Evan Goode <mail@evangoo.de>
2025-11-22 09:39:58 -0500

    bonus: Matt Debates Bryan Caplan on Poverty

--- src/isBonusTitle.ts
+++ src/isBonusTitle.ts
@@ -1,4 +1,5 @@
 const bonusTitles = new Set([
+    "Matt Debates Bryan Caplan on Poverty",
     "BONUS CONTENT: Matt talks with Unlearning Economics",
     "Bonus Content: Matt Discusses Kamala Harris's Policies on Radio Show",
     "Bonus Content: Matt Talks NBA Unfair Labor Practice, Chris Paul on Lefty Specialists",
